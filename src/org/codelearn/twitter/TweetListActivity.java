0c10989d1105cb7381f8045ee6fc010f53d9d7f2
pocha <pocha.sharma@gmail.com>
2014-02-13 19:57:51 +0530

    serializing & writing to file done

--- src/org/codelearn/twitter/TweetListActivity.java
+++ src/org/codelearn/twitter/TweetListActivity.java
@@ -1,5 +1,7 @@
 package org.codelearn.twitter;
 
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,6 +9,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
@@ -22,6 +25,7 @@ public class TweetListActivity extends ListActivity {
 
     private ArrayAdapter tweetItemArrayAdapter;
     private List<Tweet> tweets = new ArrayList<Tweet>();
+    private static final String TWEETS_CACHE_FILE = "tweet_cache.ser";
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +38,35 @@ public class TweetListActivity extends ListActivity {
 		    tweet.setBody("Some random body text for the tweet # " +i);
 		    tweets.add(tweet);
 		}
+		
+		FileOutputStream fos = null;
+	    ObjectOutputStream oos = null;
+	    
+	    try {
+	        fos = openFileOutput(TWEETS_CACHE_FILE, MODE_PRIVATE);
+	        oos = new ObjectOutputStream(fos);
+	        oos.writeObject(tweets);
+	        Log.d("codelearn", "Successfully wrote tweets to the file.");
+	    } 
+	    catch ( Exception e ) {
+
+	        Log.e("codelearn", "Failed to write to serialised file.", e);
+
+	    } finally {
+	        
+	        try {
+	            fos.close();
+	        } catch (Exception e) {
+	            Log.e("codelearn", "Error closing file stream", e);
+	        }
+	        
+	        try {
+	            oos.close();
+	        } catch (Exception e) {
+	            Log.e("codelearn", "Error closing output stream", e);
+	        }
+	    }
+
 		
 		tweetItemArrayAdapter = new TweetAdapter(this, tweets);
 		setListAdapter(tweetItemArrayAdapter);
--- src/org/codelearn/twitter/models/Tweet.java
+++ src/org/codelearn/twitter/models/Tweet.java
@@ -1,9 +1,11 @@
 package org.codelearn.twitter.models;
+import java.io.Serializable;
 
-public class Tweet {
+public class Tweet implements Serializable {
     private String id;
     private String title;
     private String body;
+    private static final long serialVersionUID = 1L;
 
     public String getId() {
         return id;
