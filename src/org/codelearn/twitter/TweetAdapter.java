edda84ca54df53c8489204f31ce3e27c41b77fdd
vikas22 <vck173@gmail.com>
2014-02-20 16:53:27 +0530

    lesson20 final code

--- src/org/codelearn/twitter/TweetAdapter.java
+++ src/org/codelearn/twitter/TweetAdapter.java
@@ -1,23 +1,35 @@
 package org.codelearn.twitter;
 
+import java.util.List;
+
 import android.app.Activity;
+import org.codelearn.twitter.models.Tweet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
-public class TweetAdapter extends ArrayAdapter {
+public class TweetAdapter extends ArrayAdapter<Tweet> {
 
 	 private LayoutInflater inflater;
+	 private List<Tweet> tweetsLocal;
      
-     public TweetAdapter(Activity activity, String[] items){
-         super(activity, R.layout.row_tweet, items);
+     public TweetAdapter(Activity activity, List<Tweet> tweets){
+         super(activity, R.layout.row_tweet, tweets);
          inflater = activity.getWindow().getLayoutInflater();
+         tweetsLocal = tweets;
      }
      
      @Override
      public View getView(int position, View convertView, ViewGroup parent){
-         return inflater.inflate(R.layout.row_tweet, parent, false);
+         View row = inflater.inflate(R.layout.row_tweet, parent, false);
+         TextView title = (TextView) row.findViewById(R.id.tweetTitle);
+         Tweet tweet = tweetsLocal.get(position);
+         title.setText(tweet.getTitle());
+         TextView body = (TextView) row.findViewById(R.id.textView2);
+         body.setText(tweet.getBody());
+         return row;
      }
 	
 }
