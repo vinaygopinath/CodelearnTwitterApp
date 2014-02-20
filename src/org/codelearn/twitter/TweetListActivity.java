56d157a3b2fae6a19a78709cc2f266cc56a78ed7
vikas22 <vck173@gmail.com>
2014-02-20 16:53:27 +0530

    lesson20 final code

--- src/org/codelearn/twitter/TweetListActivity.java
+++ src/org/codelearn/twitter/TweetListActivity.java
@@ -1,32 +1,62 @@
 package org.codelearn.twitter;
 
-import android.os.Bundle;
-import android.app.Activity;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.codelearn.twitter.models.Tweet;
+
 import android.app.ListActivity;
 import android.content.Intent;
-import android.view.LayoutInflater;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
 
-public class TweetListActivity extends ListActivity {
+public class TweetListActivity extends ListActivity{
 
 
 
     private ArrayAdapter tweetItemArrayAdapter;
-	
+    private List<Tweet> tweets = new ArrayList<Tweet>();
+    private List<Tweet> tweetsRead=new ArrayList<Tweet>();
+    private static final String TWEETS_CACHE_FILE = "tweet_cache.ser";
+    private static final long serialVersionUID = 1L;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+		super.onCreate(savedInstanceState);	
 		setContentView(R.layout.activity_tweet_list);
 		
-		tweetItemArrayAdapter = new TweetAdapter(this, new String[10]);
+		for ( int i = 0; i < 30; i++ ) {
+		    Tweet tweet = new Tweet();
+		    tweet.setTitle("A nice header for Tweet # " +i);
+		    tweet.setBody("Some random body text for the tweet # " +i);
+		    tweets.add(tweet);
+		}
+		
+		try
+		{
+		FileOutputStream fos=openFileOutput(TWEETS_CACHE_FILE, MODE_PRIVATE);
+		ObjectOutputStream oos = new ObjectOutputStream(fos);
+	    oos.writeObject(tweets);
+	    oos.close();
+	    fos.close();
+		}
+		catch(Exception e)
+		{	
+		}
+	tweetItemArrayAdapter = new TweetAdapter(this, tweets);
 		setListAdapter(tweetItemArrayAdapter);
+		Log.d("TweetRead Size In TweetListActivity",""+tweetsRead.size());
+	
+		
 	}
 
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -35,8 +65,10 @@ public class TweetListActivity extends ListActivity {
 	}
 
 	@Override
-	 protected void onListItemClick(ListView l, View v, int position, long id) {
+	protected void onListItemClick(ListView l, View v, int position, long id) {
 		Intent intent = new Intent(this, TweetDetailActivity.class);
-	     startActivity(intent);
-	 }
+		
+		startActivity(intent);
+		
+	}
 }
