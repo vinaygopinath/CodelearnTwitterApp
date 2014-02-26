440cdc97869ae2e6f47b228466f43b6ed7e18a9c
vikas22 <vck173@gmail.com>
2014-02-27 00:48:50 +0530

    partial commit

---
+++ src/org/codelearn/twitter/models/AsyncFetchTweets.java
@@ -0,0 +1,16 @@
+package org.codelearn.twitter.models;
+
+import java.util.List;
+
+import android.database.CursorJoiner.Result;
+import android.os.AsyncTask;
+
+public class AsyncFetchTweets extends  AsyncTask<List<Tweet>, Void, Void> {
+
+	@Override
+	protected Void doInBackground(List<Tweet>... params) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
