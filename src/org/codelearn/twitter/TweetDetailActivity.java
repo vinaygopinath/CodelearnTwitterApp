6c878d9ca351d4c17a9c090edf24e088599d2028
vikas22 <vck173@gmail.com>
2014-02-27 14:56:09 +0530

    final commit lesson25

--- src/org/codelearn/twitter/TweetDetailActivity.java
+++ src/org/codelearn/twitter/TweetDetailActivity.java
@@ -1,8 +1,14 @@
 package org.codelearn.twitter;
 
-import android.os.Bundle;
+import java.util.List;
+
+import org.codelearn.twitter.models.Tweet;
+
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class TweetDetailActivity extends Activity {
 
@@ -10,6 +16,13 @@ public class TweetDetailActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tweet_detail);
+	Tweet  value = (Tweet) getIntent().getSerializableExtra("MyClass");
+		TextView u1=(		TextView) findViewById(R.id.tweetTitle);
+		
+		TextView u2=(		TextView) findViewById(R.id.tweetBody);
+		u1.setText(value.getTitle().toString());
+		u2.setText(value.getBody().toString());
+		
 	}
 
 	@Override
